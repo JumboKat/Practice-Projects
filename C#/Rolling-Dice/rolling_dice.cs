28b9cd294f41dfc8eabf8525b72bcd02b29a7baa
Keith <keithtran02@gmail.com>
2026-01-26 13:57:43 -0500

    Rolling Dice Exercise

---
+++ C#/Rolling-Dice/rolling_dice.cs
@@ -0,0 +1,53 @@
+﻿/*
+In this exercise, you'll practice writing if statements by creating a game. First you'll define the rules of the game, then you'll implement them in code.
+
+You'll use the Random.Next() method to simulate rolling three six-sided dice. You'll evaluate the rolled values to calculate the score. If the score is greater than an arbitrary total, then you'll display a winning message to the user. If the score is below the cutoff, you'll display a losing message to the user.
+
+    If any two dice you roll result in the same value, you get two bonus points for rolling doubles.
+    If all three dice you roll result in the same value, you get six bonus points for rolling triples.
+    If the sum of the three dice rolls, plus any point bonuses, is 15 or greater, you win the game. Otherwise, you lose.
+*/
+
+// Setup
+
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || roll2 == roll3 || roll1 == roll3)
+{
+    if (roll1 == roll2 && roll2 == roll3)
+    {
+        Console.WriteLine("You rolled triples! +6 bonus to total!");
+        total +=6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles! +2 bonus to total!");
+        total +=2;
+    }
+}
+
+
+if (total >=16)
+{
+    Console.WriteLine("You win a new car!");
+}
+else if (total >= 10)
+{
+    Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
+}
