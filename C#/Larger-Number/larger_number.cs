8fc6ff97a09d33d992e50b41cbe01708df8015cf
Keith <keithtran02@gmail.com>
2026-01-25 20:51:38 -0500

    Larger Number exercise

---
+++ C#/Larger-Number/larger_number.cs
@@ -0,0 +1,9 @@
+﻿// In this challenge, you'll use either Intellisense or learn.microsoft.com to find and call a method that returns the larger of two numbers.
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+
+largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
