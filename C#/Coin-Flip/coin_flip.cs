afba4964358199d299600493d15a294d99140274
Keith <keithtran02@gmail.com>
2026-01-28 16:11:50 -0500

    Coin Flip Exercise

---
+++ C#/Coin-Flip/coin_flip.cs
@@ -0,0 +1,7 @@
+﻿/*
+In this challenge, you'll implement a conditional operator to simulate a "coin flip." The resulting decision logic will display either heads or tails.
+*/
+
+Random random = new Random();
+
+Console.WriteLine($"{(random.Next(1, 3)==1 ? "heads" : "tails")}");
